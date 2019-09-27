17076c28323181ae3c5dfa48aeb45f5d57200b6a
BryanNilsen <bryannilsen@gmail.com>
2019-09-26 22:53:42 -0500

    completed stocks exercise

---
+++ dict-stocks/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace stocks
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Dictionary<string, string> stocks = new Dictionary<string, string>();
+      stocks.Add("GM", "General Motors");
+      stocks.Add("BRY", "Berry Petroleum Corp.");
+      stocks.Add("AAPL", "Apple");
+      stocks.Add("VZ", "Verizon");
+
+
+      Dictionary<string, List<double>> portfolio = new Dictionary<string, List<double>>();
+      portfolio.Add("GM", new List<double>() { { 1214.90 }, { 4892.12 }, { 984.98 } });
+      portfolio.Add("AAPL", new List<double>() { { 3764.33 }, { 1812.41 } });
+      portfolio.Add("BRY", new List<double>() { { 3764.33 }, { 1812.41 } });
+      portfolio.Add("VZ", new List<double>() { { 104.20 }, { 536.02 } });
+
+      foreach (KeyValuePair<string, List<double>> purchase in portfolio)
+      {
+        string companyName = "";
+        foreach (KeyValuePair<string, string> company in stocks)
+        {
+          if (company.Key == purchase.Key)
+          {
+            companyName = company.Value;
+          }
+        }
+
+        double sum = 0;
+        foreach (double price in purchase.Value)
+        {
+          sum += price;
+        }
+        Console.WriteLine($"Investor has spent a total of ${sum} on the ({purchase.Key}) stock for {companyName}");
+      };
+    }
+  }
+}
