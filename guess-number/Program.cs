4008eb6e7a907d78c53380f8afbbd8c1a6b16e1d
BryanNilsen <bryannilsen@gmail.com>
2019-09-29 11:42:26 -0500

    completed phase 1

---
+++ guess-number/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace guess_number
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string guess;
+      Console.Write("Guess a Number: ");
+      guess = Console.ReadLine();
+
+      // convert string to integer
+      int num = Convert.ToInt32(guess);
+      Console.WriteLine("Your guess: {0}", num);
+    }
+  }
+}
