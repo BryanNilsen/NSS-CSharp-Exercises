8d0e0b292b547d91e15f3999113d15163cb192b3
BryanNilsen <bryannilsen@gmail.com>
2019-09-29 12:56:09 -0500

    completed phase 6

--- guess-number/Program.cs
+++ guess-number/Program.cs
@@ -15,7 +15,7 @@ namespace guess_number
       int secret = random.Next(0, 100);
 
 
-      while (guessCount <= 4)
+      while (guessCount < 4)
       {
         string guess;
         Console.Write($"Guesses left({4 - guessCount}) - Guess a number between 1 - 100: ");
@@ -29,16 +29,23 @@ namespace guess_number
         if (num == secret)
         {
           Console.WriteLine("You guessed correctly!");
-          guessCount = 5;
+          guessCount = 4;
         }
         else
         {
-          Console.WriteLine("Sorry, try again.");
+          if (num >= secret)
+          {
+            Console.WriteLine("Sorry, that's too high.");
+          }
+          else
+          {
+            Console.WriteLine("Sorry, that's too low.");
+          }
           guessCount++;
         }
       }
-
-      if (guessCount == 5)
+      // display secret number when game over
+      if (guessCount == 4)
       {
         Console.WriteLine($"The secret number was {secret}");
       }
